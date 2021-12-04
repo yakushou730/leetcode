d8b937536b8dcd703bf86488c3a1a15bd2a69ccb
Shou Tseng <yakushou730@gmail.com>
2021-12-04 10:36:47 +0800

    feat: update 13. Roman to Integer

--- easy/13-roman-to-integer/main.go
+++ easy/13-roman-to-integer/main.go
@@ -1,52 +1,81 @@
 package main
 
-import "fmt"
-
 // https://leetcode.com/problems/roman-to-integer/
+
 func romanToInt(s string) int {
 	romanMap := map[string]int{
-		"I":  1,
-		"V":  5,
-		"X":  10,
-		"L":  50,
-		"C":  100,
-		"D":  500,
-		"M":  1000,
-		"IV": 4,
-		"IX": 9,
-		"XL": 40,
-		"XC": 90,
-		"CD": 400,
-		"CM": 900,
+		"I": 1,
+		"V": 5,
+		"X": 10,
+		"L": 50,
+		"C": 100,
+		"D": 500,
+		"M": 1000,
 	}
 
-	var stringArr []string
-	var tempStr string
-
-	for _, ch := range s {
-		str := string(rune(ch))
-		if tempStr == "" {
-			tempStr = str
-			continue
+	sum := 0
+	var prevStr string
+	for i := 1; i <= len(s); i++ {
+		currStr := string(s[len(s)-i])
+		if prevStr == "" {
+			sum += romanMap[currStr]
 		} else {
-			if romanMap[tempStr] < romanMap[str] {
-				stringArr = append(stringArr, fmt.Sprintf("%s%s", tempStr, str))
-				tempStr = ""
+			if romanMap[currStr] < romanMap[prevStr] {
+				sum -= romanMap[currStr]
 			} else {
-				stringArr = append(stringArr, tempStr)
-				tempStr = str
+				sum += romanMap[currStr]
 			}
 		}
-	}
-
-	if tempStr != "" {
-		stringArr = append(stringArr, tempStr)
-	}
-
-	var sum int
-	for _, str := range stringArr {
-		sum += romanMap[str]
+		prevStr = currStr
 	}
 
 	return sum
 }
+
+//func romanToInt(s string) int {
+//	romanMap := map[string]int{
+//		"I":  1,
+//		"V":  5,
+//		"X":  10,
+//		"L":  50,
+//		"C":  100,
+//		"D":  500,
+//		"M":  1000,
+//		"IV": 4,
+//		"IX": 9,
+//		"XL": 40,
+//		"XC": 90,
+//		"CD": 400,
+//		"CM": 900,
+//	}
+//
+//	var stringArr []string
+//	var tempStr string
+//
+//	for _, ch := range s {
+//		str := string(rune(ch))
+//		if tempStr == "" {
+//			tempStr = str
+//			continue
+//		} else {
+//			if romanMap[tempStr] < romanMap[str] {
+//				stringArr = append(stringArr, fmt.Sprintf("%s%s", tempStr, str))
+//				tempStr = ""
+//			} else {
+//				stringArr = append(stringArr, tempStr)
+//				tempStr = str
+//			}
+//		}
+//	}
+//
+//	if tempStr != "" {
+//		stringArr = append(stringArr, tempStr)
+//	}
+//
+//	var sum int
+//	for _, str := range stringArr {
+//		sum += romanMap[str]
+//	}
+//
+//	return sum
+//}
