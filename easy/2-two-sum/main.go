92cf25e046a0d130fae397374b80c22394328fc9
Shou Tseng <yakushou730@gmail.com>
2021-12-05 22:46:17 +0800

    feat: update 1. Two Sum

--- easy/2-two-sum/main.go
+++ easy/2-two-sum/main.go
@@ -2,25 +2,40 @@ package main
 
 // https://leetcode.com/problems/two-sum/
 func twoSum(nums []int, target int) []int {
-	var firstIndex, secondIndex int
-	ok := false
+	numsMap := make(map[int]int)
+	for i, v := range nums {
+		numsMap[v] = i
+	}
+	var oneOfItem int
 	for i := 0; i < len(nums); i++ {
-		firstItem := nums[i]
-		for j := i + 1; j < len(nums); j++ {
-			secondItem := nums[j]
-			if firstItem+secondItem != target {
-				continue
-			} else {
-				ok = true
-				firstIndex = i
-				secondIndex = j
-				break
-			}
-		}
-		if ok {
-			break
+		oneOfItem = target - nums[i]
+		if _, ok := numsMap[oneOfItem]; ok && numsMap[oneOfItem] != i {
+			return []int{i, numsMap[oneOfItem]}
 		}
 	}
-
-	return []int{firstIndex, secondIndex}
+	return []int{}
 }
+
+// func twoSum(nums []int, target int) []int {
+// 	var firstIndex, secondIndex int
+// 	ok := false
+// 	for i := 0; i < len(nums); i++ {
+// 		firstItem := nums[i]
+// 		for j := i + 1; j < len(nums); j++ {
+// 			secondItem := nums[j]
+// 			if firstItem+secondItem != target {
+// 				continue
+// 			} else {
+// 				ok = true
+// 				firstIndex = i
+// 				secondIndex = j
+// 				break
+// 			}
+// 		}
+// 		if ok {
+// 			break
+// 		}
+// 	}
+//
+// 	return []int{firstIndex, secondIndex}
+// }
