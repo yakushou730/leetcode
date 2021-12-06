bf498d57895854e566d6b3a62385768d2828b225
Shou Tseng <yakushou730@gmail.com>
2021-12-06 20:46:14 +0800

    feat: update Find Numbers with Even Number of Digits

--- explore/array/find-numbers-with-even-number-of-digits/main.go
+++ explore/array/find-numbers-with-even-number-of-digits/main.go
@@ -2,6 +2,7 @@ package main
 
 import "strconv"
 
+// https://leetcode.com/explore/learn/card/fun-with-arrays/521/introduction/3237/
 func findNumbers(nums []int) int {
 	sum := 0
 	for _, num := range nums {
