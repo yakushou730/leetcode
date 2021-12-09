5b2be3f6d340fdad5475b5549da8654977ae17f7
Shou Tseng <yakushou730@gmail.com>
2021-12-09 09:51:44 +0800

    feat: update 977. Squares of a Sorted Array

--- easy/977-squares-of-a-sorted-array/main.go
+++ easy/977-squares-of-a-sorted-array/main.go
@@ -1,24 +1,43 @@
 package main
 
 func sortedSquares(nums []int) []int {
-	var result []int
+	result := make([]int, len(nums))
 	i := 0
 	j := len(nums) - 1
-	for k := 0; k < len(nums); k++ {
+	for k := j; k >= 0; k-- {
 		squaredI := nums[i] * nums[i]
 		squaredJ := nums[j] * nums[j]
 		if squaredI > squaredJ {
-			result = append(result, squaredI)
+			result[k] = squaredI
 			i++
 		} else {
-			result = append(result, squaredJ)
+			result[k] = squaredJ
 			j--
 		}
 	}
 
-	for i, j := 0, len(result)-1; i < j; i, j = i+1, j-1 {
-		result[i], result[j] = result[j], result[i]
-	}
-
 	return result
 }
+
+//func sortedSquares(nums []int) []int {
+//	var result []int
+//	i := 0
+//	j := len(nums) - 1
+//	for k := 0; k < len(nums); k++ {
+//		squaredI := nums[i] * nums[i]
+//		squaredJ := nums[j] * nums[j]
+//		if squaredI > squaredJ {
+//			result = append(result, squaredI)
+//			i++
+//		} else {
+//			result = append(result, squaredJ)
+//			j--
+//		}
+//	}
+//
+//	for i, j := 0, len(result)-1; i < j; i, j = i+1, j-1 {
+//		result[i], result[j] = result[j], result[i]
+//	}
+//
+//	return result
+//}
